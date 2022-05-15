6045f18d7216c3f9553ab2697e0dbef73ba33d04
Md Mazharul islam <miforbd@gmail.com>
2022-05-15 21:37:49 +0600

    booking and userinfo added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port=process.env.PORT||5000;
@@ -17,15 +18,36 @@ async function run(){
     try{
         await client.connect();
         const treatmentCollection=client.db('treatment').collection('service');
-        
+        const bookingCollection=client.db('treatment').collection('booking');
+        const userCollection=client.db('treatment').collection('user');
+        // all service show
         app.get('/service',async (req,res)=>{
             const query={};
             const cursor=treatmentCollection.find(query);
             const services=await cursor.toArray();
             res.send(services);
         });
+        // user info
+        app.put('/user/:email',async(req,res)=>{
+            const email=req.params.email;
+            const user=req.body;
+            const filter={email: email};
+            const options={upsert:true};
+            const updateDoc={
+                $set:user,
+            };
+            const result=await userCollection.updateOne(filter,updateDoc,options);
+            const token=jwt.sign({email:email},process.env.ACCESS_TOKEN,{expiresIn:'1d'})
+            res.send({result,accessToken:token});
+        })
+        // booking api
+        app.post('/booking',async(req,res)=>{
+            const booking=req.body;
+            const result=await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
     }
-    
+
     
     finally{
 
